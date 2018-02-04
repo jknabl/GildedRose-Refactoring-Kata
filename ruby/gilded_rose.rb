845ae5d248cc2cd4133480ecd8aac14c5e907c3a
Jason Knabl <jason.knabl@gmail.com>
2018-02-04 10:57:18 -0500

    All update logic extracted out of update_quality method.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,56 +4,55 @@ class GildedRose
     @items = items
   end
 
+  def update_aged_brie(item)
+    item.quality += 1 if item.quality < 50
+    
+    item.sell_in -= 1
+
+    item.quality -= 1 if (item.sell_in < 0 && item.quality > 50)
+  end
+
+  def update_backstage_passes(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.sell_in < 11
+        item.quality += 1 if item.quality < 50
+      end
+      if item.sell_in < 6
+        item.quality += 1 if item.quality < 50
+      end
+    end
+
+    item.sell_in -= 1
+
+    item.quality = 0 if item.sell_in < 0
+  end
+  
+  def update_sulfuras(item)
+    item.sell_in -= 1
+  end
+
+  def update_generic(item)
+    item.quality -= 1 if item.quality > 0
+
+    item.sell_in -= 1
+
+    if item.sell_in < 0
+      item.quality -= 1 if item.quality > 0
+    end
+  end
+
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      case item.name
+      when 'Aged Brie'
+        update_aged_brie(item)
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        update_backstage_passes(item)
+      when 'Sulfuras, Hand of Ragnaros'
+        update_sulfuras(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-
-      if item.sell_in < 0
-        # Post-sell-by-date penalties
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                # Generic penalty
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            # Backstage pass penalty
-            item.quality = item.quality - item.quality
-          end
-        else
-          # Aged brie age boost
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        update_generic(item)
       end
     end
   end
